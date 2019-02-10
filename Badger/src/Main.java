0664fba55af99205a8cb3611537037621500bb3a
komodochristopher <flashdog1>
2019-02-10 12:59:19 -0800

    Badgers

---
+++ Badger/src/Main.java
@@ -0,0 +1,67 @@
+
+public class Main {
+public static void main(String[] args) {
+	for(int i=0;i<12;i++) {
+		System.out.println("badger");
+		
+	}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+	}
+	for(int i=0;i<9;i++) {
+		System.out.println("badger");
+	}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+	}
+	for(int i=0;i<9;i++) {
+		System.out.println("badger");
+	}
+	for(int i=0;i<1;i++) {
+		System.out.println("mush-mushroom");
+}
+	for(int i=0;i<11;i++) {
+		System.out.println("badger");
+}
+	for(int i=0;i<4;i++) {
+		System.out.println("A snaaake");
+}
+	for(int i=0;i<1;i++) {
+		System.out.println("Oooh, it's a snake");
+}
+	for(int i=0;i<12;i++) {
+		System.out.println("badger");
+}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+}
+	for(int i=0;i<12;i++) {
+		System.out.println("badger");
+}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+}
+	for(int i=0;i<11;i++) {
+		System.out.println("badger");
+}
+	for(int i=0;i<2;i++) {
+		System.out.println("mushroom");
+}
+	for(int i=0;i<11;i++) {
+		System.out.println("badger");
+	}
+	for(int i=0;i<1;i++) {
+		System.out.println("mush-mushroom");
+
+}
+	for(int i=0;i<11;i++) {
+		System.out.println("badger");
+}
+	for(int i=0;i<4;i++) {
+		System.out.println("A snaaake");
+}
+	for(int i=0;i<1;i++) {
+		System.out.println("Oooh, it's a snake");
+}
+}
+}
